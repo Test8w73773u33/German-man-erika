00a5c2548ebe4d7bf70737bff6ed8b00d0e4a183
Test8w73773u33 <forbesshane232@gmail.com>
2026-03-13 20:47:22 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
-    lebron,
     // You can add multiple users by doing username: 'password'.
-    interstellar: "germanpaint-erika",
+    hitler: "germanpaint-erika",
+    john: "Johnloveskids",
   },
 };
 
